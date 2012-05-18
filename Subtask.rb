5e2795ea0ed04b426616b84c2e9d3a4cda3eae1b
Ihor Breza <seniorigor@gmail.com>
2012-05-18 14:58:41 +0300

    Add Subtask (empty)

---
+++ Subtask.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# Author: seniorihor
+
+require_relative './Task.rb'
+
+class Subtask
+
+  def initialize
+    #code
+  end
+
+  def title
+    #code
+  end
+
+  def title=(string)
+    #code
+  end
+
+  def start_time
+    #code
+  end
+
+  def end_time
+    #code
+  end
+
+  def end_time=(time)
+    #code
+  end
+
+  def priority=(number)
+    #code
+  end
+
+end
